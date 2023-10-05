0636c6f1f78411e38f1bcafdcb93ff3dac7c2629
Ubuntu <ubuntu@ip-172-31-38-32.ap-northeast-2.compute.internal>
2023-10-05 04:13:27 +0000

    ASE: argument passing - 파싱 부분 완료

--- do_userprog_gdb.sh
+++ do_userprog_gdb.sh
@@ -3,5 +3,5 @@ make clean
 make
 cd build
 source ../../activate
-# pintos-mkdisk filesys.dsk 10
+pintos-mkdisk filesys.dsk 10
 pintos --gdb --fs-disk filesys.dsk -p tests/userprog/args-single:args-single -- -q -f run 'args-single onearg'
